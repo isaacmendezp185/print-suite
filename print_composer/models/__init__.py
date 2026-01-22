b7221ad93ccc2d00c9aca6dc93d34e18c6919f6f
isaacmendezp185 <isaacmendezp@gmail.com>
2026-01-22 05:18:50 +0000

    Add sale_order_inherit import to __init__.py

--- print_composer/models/__init__.py
+++ print_composer/models/__init__.py
@@ -1,4 +1,7 @@
 from . import composer_wizard
-from . import product_substrate_inherit    # nuevo
-from . import print_job_inherit            # nuevo
-from . import sale_order_line_inherit      # nuevo
+from . import product_substrate_inherit
+from . import print_job_inherit
+from . import sale_order_line_inherit
+from . import sale_order_inherit     # <-- NUEVO
+
+
