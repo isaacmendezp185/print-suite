4332805e69c4b64dcf4aa9c89575d4cd17220486
isaacmendezp185 <isaacmendezp@gmail.com>
2026-01-22 05:23:34 +0000

    Implement action to open linked sale order
    Add method to open linked sale order from print job

--- print_composer/models/print_job_inherit.py
+++ print_composer/models/print_job_inherit.py
@@ -1,8 +1,23 @@
 
-# print_composer/models/print_job_inherit.py
-from odoo import fields, models
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError
 
 class PrintJob(models.Model):
     _inherit = 'print.job'
 
     sale_line_id = fields.Many2one('sale.order.line', string='Línea de venta', index=True)
+
+    def action_open_sale_order(self):
+        """Abre la cotización/pedido de venta vinculado a este trabajo."""
+        self.ensure_one()
+        if not self.sale_line_id or not self.sale_line_id.order_id:
+            raise UserError(_('Este trabajo no está vinculado a ninguna cotización/pedido.'))
+        order = self.sale_line_id.order_id
+        return {
+            'type': 'ir.actions.act_window',
+            'name': _('Pedido de venta'),
+            'res_model': 'sale.order',
+            'res_id': order.id,
+            'view_mode': 'form',
+            'target': 'current',
+        }
